9ab3868a1d84ca1ecea93f5015738ba8cb92d06e
trizo1104 <vominhtri221104@gmail.com>
2025-09-26 00:54:06 +0700

    implement home screen

--- src/layouts/default-layout.tsx
+++ src/layouts/default-layout.tsx
@@ -1,8 +1,9 @@
 import { Helmet, HelmetProvider } from 'react-helmet-async';
-
 import { Navbar03 } from '@/components/ui/navbar';
-
 import type { Meta } from '@/types';
+import { useLocation } from '@tanstack/react-router';
+import HeroSection from '@/components/ui/hero-section';
+import Footer from '@/components/ui/footer';
 
 interface DefaultLayoutProps {
   children: React.ReactNode;
@@ -10,6 +11,9 @@ interface DefaultLayoutProps {
 }
 
 export const DefaultLayout = ({ children, meta }: DefaultLayoutProps) => {
+  const location = useLocation();
+
+  console.log('Current location:', location);
   return (
     <HelmetProvider>
       <Helmet>
@@ -18,7 +22,10 @@ export const DefaultLayout = ({ children, meta }: DefaultLayoutProps) => {
       </Helmet>
       <div>
         <Navbar03 />
+        {location.pathname === '/' && <HeroSection />}
+
         <main className="container mx-auto p-8">{children}</main>
+        {location.pathname === '/' && <Footer />}
       </div>
     </HelmetProvider>
   );
