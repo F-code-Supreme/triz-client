5126d87d7b862f76c1d9e8534422d86c3869dc35
trtien05 <trantien100700@gmail.com>
2025-10-13 15:18:52 +0700

    TRIZ-100 fix: delete triz principles routes

--- src/routes/triz-principles.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router';
-
-export const Route = createFileRoute('/triz-principles')({
-  component: RouteComponent,
-});
-
-const RouteComponent = () => {
-  return <div>Hello "/triz-principles"!</div>;
-};
