ec7a3da8e22449c4d5c5314f4d0483a46334bf19
trizo1104 <vominhtri221104@gmail.com>
2025-11-05 13:52:31 +0700

    adjust feature quiz

--- src/routes/quiz.history.index.tsx
+++ src/routes/quiz.history.index.tsx
@@ -1,4 +1,4 @@
-import HistoryPage from '@/pages/main/quiz/History';
+import HistoryPage from '@/pages/main/quiz/history';
 import { createFileRoute } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/quiz/history/')({
