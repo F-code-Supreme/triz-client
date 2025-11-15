2b28ed7c9b68e0137eab41e98b71232b3aeb28d8
baoha7204 <baoha3604@gmail.com>
2025-11-15 22:17:32 +0700

    TRIZ-173 feat: add shadcn-minimal-tiptap, fix public query package services, remove TanStack Router Devtools

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,5 +1,4 @@
 import { createRootRouteWithContext, Outlet } from '@tanstack/react-router';
-import { TanStackRouterDevtools } from '@tanstack/router-devtools';
 
 import NotFoundPage from '@/pages/global/not-found';
 
@@ -13,7 +12,6 @@ export const Route = createRootRouteWithContext<AppRouterContext>()({
   component: () => (
     <>
       <Outlet />
-      <TanStackRouterDevtools />
     </>
   ),
   notFoundComponent: NotFoundPage,
