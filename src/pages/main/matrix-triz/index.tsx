bfe3455c6f2d4bc66ba145fc8a038d9d9ffbdbfa
trtien05 <trantien100700@gmail.com>
2025-11-24 21:14:35 +0700

    refactor(matrix-triz): remove unused imports and clean up code

--- src/pages/main/matrix-triz/index.tsx
+++ src/pages/main/matrix-triz/index.tsx
@@ -1,12 +1,7 @@
-import React from 'react';
-
 import { MatrixSection } from '@/components/ui/matrix-triz';
 import { DefaultLayout } from '@/layouts/default-layout';
-import { TrizMatrix } from '@/pages/main/matrix-triz/components/triz-matrix';
 
 const MatrixTriz = () => {
-  
-
   return (
     <DefaultLayout meta={{ title: 'TRIZ Matrix' }} className="">
       <MatrixSection />
