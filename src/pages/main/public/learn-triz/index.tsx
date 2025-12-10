155110c1e8f848ef81c6a0baa7379127fa85ffc5
baoha7204 <baoha3604@gmail.com>
2025-12-10 22:17:41 +0700

    feat: add meta title for learn triz and matrix

--- src/pages/main/public/learn-triz/index.tsx
+++ src/pages/main/public/learn-triz/index.tsx
@@ -1,12 +1,16 @@
+import { useTranslation } from 'react-i18next';
+
 import { PrincipleSection } from '@/components/ui/principle-hero-section';
 import { DefaultLayout } from '@/layouts/default-layout';
 
 import { TRIZPrinciplesList } from './components/triz-principles-list';
 
 const LearnTRIZPage = () => {
+  const { t } = useTranslation('pages.learn_triz');
+
   return (
     <DefaultLayout
-      meta={{ title: '40 Nguyên Tắc Sáng Tạo' }}
+      meta={{ title: t('page_meta_title') }}
       className="scroll-smooth"
     >
       <PrincipleSection />
