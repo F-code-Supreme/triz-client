a01b382fccac870ef000762b1d174eff6f539882
baoha7204 <baoha3604@gmail.com>
2025-12-12 18:39:26 +0700

    TRIZ-226 feat: add achievement services

---
+++ src/features/achievement/services/queries/keys.ts
@@ -0,0 +1,8 @@
+export enum AchievementKeys {
+  GetActiveAchievementsQuery = 'achievement/getActiveAchievements',
+  GetAchievementsQuery = 'achievement/getAchievements',
+  GetDeletedAchievementsQuery = 'achievement/getDeletedAchievements',
+  GetAchievementByIdQuery = 'achievement/getAchievementById',
+  GetUserAchievementsQuery = 'achievement/getUserAchievements',
+  GetUserUnreadAchievementsQuery = 'achievement/getUserUnreadAchievements',
+}
