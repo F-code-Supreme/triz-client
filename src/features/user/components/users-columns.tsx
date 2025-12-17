a26e42a5d307059f681687842710da8646a75951
baoha7204 <baoha3604@gmail.com>
2025-12-17 16:19:03 +0700

    feat: add role color for moderator

--- src/features/user/components/users-columns.tsx
+++ src/features/user/components/users-columns.tsx
@@ -13,6 +13,7 @@ export const roleColors: Record<RoleIUser, string> = {
   [RoleIUser.USER]: 'bg-blue-100 text-blue-800 hover:bg-blue-100/90',
   [RoleIUser.ADMIN]: 'bg-red-100 text-red-800 hover:bg-red-100/90',
   [RoleIUser.EXPERT]: 'bg-purple-100 text-purple-800 hover:bg-purple-100/90',
+  [RoleIUser.MODERATOR]: 'bg-green-100 text-green-800 hover:bg-green-100/90',
 };
 
 export const usersColumns: ColumnDef<IUser>[] = [
